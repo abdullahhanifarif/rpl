7af71e36d597553bb4c4d076c922b55d49571637
Abdullah Hanif Arif <96676918+abdullahhanifarif@users.noreply.github.com>
2024-03-26 14:34:47 +0700

    Update primeNumber.test.js

--- primeNumber.test.js
+++ primeNumber.test.js
@@ -1,73 +1,42 @@
 const isPrime = require('./primeNumber');
 
-
-describe('Pengecekan Bilangan Prima', () => {
-    test('2 adalah bilangan prima', () => {
+describe('Pengecekan angka yang merupakan Bilangan Prima', () => {
+    test('2, 3, 7 adalah bilangan prima', () => {
         //Given
-        const primeNumber = 2;
+        const primeNumber1 = 2;
+        const primeNumber2 = 3;
+        const primeNumber3 = 7;
 
         //When
-        const result = isPrime(primeNumber);
+        const result1 = isPrime(primeNumber1);
+        const result2 = isPrime(primeNumber2);
+        const result3 = isPrime(primeNumber3);
 
         //Then
-        expect(result).toBe(true);
-    });
-    test('3 adalah bilangan prima', () => {
-        //Given
-        const primeNumber = 3;
-
-        //When
-        const result = isPrime(primeNumber);
-
-        //Then
-        expect(result).toBe(true);
-    });
-    test('4 bukan bilangan prima', () => {
-        //Given
-        const primeNumber = 4;
-
-        //When
-        const result = isPrime(primeNumber);
-
-        //Then
-        expect(result).toBe(false);
-    });
-    test('7 adalah bilangan prima', () => {
-        //Given
-        const primeNumber = 7;
-
-        //When
-        const result = isPrime(primeNumber);
-
-        //Then
-        expect(result).toBe(true);
-    });
-    test('25 bukan bilangan prima', () => {
-        //Given
-        const primeNumber = 25;
-
-        //When
-        const result = isPrime(primeNumber);
-
-        //Then
-        expect(result).toBe(false);
+        expect(result1).toBe(true);
+        expect(result2).toBe(true);
+        expect(result3).toBe(true);
     });
 });
 
+describe('Pengecekan angka yang bukan merupakan Bilangan Prima', () =>{
+    test('4 dan 25 bukan bilangan prima', () => {
+        //Given
+        const primeNumber1 = 4;
+        const primeNumber2 = 25;
+
+        //When
+        const result1 = isPrime(primeNumber1);
+        const result2 = isPrime(primeNumber2);
+
+        //Then
+        expect(result1).toBe(false);
+        expect(result2).toBe(false);
+    });
+});
 
 describe('Pengecekan tipe data', () => {
-    // test('tiga adalah bilangan prima', () => {
-    //     //Given
-    //     const primeNumber = 'tiga';
-
-    //     //When
-    //     const result = isPrime(primeNumber);
-
-    //     //Then
-    //     expect(result).toBe(true);
-    // });
-
-    test('tiga adalah string(inputan salah)', () => {
+    test('tiga adalah bilangan prima', () => {
         //Given
         const primeNumber = 'tiga';
 
@@ -75,6 +44,17 @@ describe('Pengecekan tipe data', () => {
         const result = isPrime(primeNumber);
 
         //Then
-        expect(result).toBe('inputannya salah');
+        expect(result).toBe(true);
+    });
+
+    test('tiga adalah bukan angka)', () => {
+        //Given
+        const primeNumber = 'tiga';
+
+        //When
+        const result = isPrime(primeNumber);
+
+        //Then
+        expect(result).toBe("it's not a number");
     });
 });
